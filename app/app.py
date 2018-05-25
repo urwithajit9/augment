cc53c340593b099f37e93f35896c04ecc4cc63ac
Ajit Kumar <ajitkumar.pu@gmail.com>
2018-05-25 14:44:49 +0530

    added port and IP or app.run()

--- app/app.py
+++ app/app.py
@@ -164,5 +164,6 @@ def create_classification():
                     }), 201
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__':    
+    port = int(os.environ.get('PORT', 33507))
+    app.run(host="0.0.0.0", port=port,debug=True)
